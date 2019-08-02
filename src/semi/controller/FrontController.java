e513f8a4a4e247fd98630168b614dfe4503ad0ea
JongMinLee <1393062@naver.com>
2019-08-02 11:15:13 +0900

    Add Front Controller
    Start project And load index.jsp

---
+++ src/semi/controller/FrontController.java
@@ -0,0 +1,30 @@
+package semi.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+// 메인 페이지 로드
+@WebServlet("/semi")
+public class FrontController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		execute(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		execute(req, resp);
+	}
+	
+	protected void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dis = req.getRequestDispatcher("index.jsp");
+		dis.forward(req, resp);
+	}
+}
